5d67db2ba346374dd1b8cc3c6f7b626392862209
AbdumajidovAvazbek <abdumajidova51@gmail.com>
2023-11-07 07:48:10 -0800

    add Exseption

--- src/CashFlow.Service/Configurations/PaginationMetaData.cs
+++ src/CashFlow.Service/Configurations/PaginationMetaData.cs
@@ -2,7 +2,7 @@
 
 public class PaginationMetaData
 {
-    public int CurrentPage { get; set; }       /
+    public int CurrentPage { get; set; }       
     public int TotalPages { get; set; }         
     public int TotalCount { get; set; }        
     public bool HasPrevious => CurrentPage > 1;
---
+++ src/CashFlow.Service/Exceptions/CashFlowExseption.cs
@@ -0,0 +1,11 @@
+﻿namespace CashFlow.Service.Exceptions;
+
+public class CashFlowExseption : Exception
+{
+    public int _statusCode {  get; set; }
+
+    public CashFlowExseption(int code,string message) : base (message)
+    {
+        _statusCode = code;
+    }
+}
